935d5784609ae0c7f2ae7e164fedacbf1255bcd0
Jason <jakiracofe10@gmail.com>
2016-12-10 14:34:40 -0600

    "fix overflow logic"

--- main.cpp
+++ main.cpp
@@ -3,17 +3,25 @@
 using namespace std;
 
 void addOvf(int *result, int a, int b) {
-    if (a > INT64_MAX-b) {
-        *result = -1;
-    } else {
-        *result = a+b;
+    int added = a+b;
+    if (a > 0 && b > 0) {
+        if (added < 0) {
+            *result = -1;
+            return;
+        }
+    } else if (a < 0 && b < 0) {
+        if (added > 0) {
+            *result = -1;
+            return;
+        }
     }
+    *result = a + b;
 }
 
 int main() {
     int result[1];
-    int a = 9223372036854775807;
-    int b = 9223372036854775807;
+    int a = 1200000000;
+    int b = 1200000000;
     addOvf(result, a, b);
     cout << *result << endl;
     return 0;
